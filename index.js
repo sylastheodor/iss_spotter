6d831994a080f08e5164ba899a9a7b43c81ad624
Sylas Serné <its.sylas@gmail.com>
2021-07-02 04:34:24 +0000

    I have the logic for the big function working now I just have to make it pretty in the output department

--- index.js
+++ index.js
@@ -1,9 +1,52 @@
-const { fetchMyIP } = require('./iss');
+const { fetchMyIP, fetchCoordsByIP, fetchISSFlyOverTimes, nextISSTimesForMyLocation } = require('./iss');
 
-fetchMyIP((err, IP) => {
-  if(err) {
-    console.log(err)
+// fetchMyIP((err, IP) => {
+//   if (err) {
+//     console.log(err);
+//   } else {
+//     console.log(IP);
+//   }
+// });
+
+// fetchCoordsByIP('154.20.198.133', (err, location) => {
+//   if (err) {
+//     console.log(err);
+//   } else {
+//     console.log(location);
+//   }
+// })
+
+// const coords = {longitude: -123.3047, latitude: 48.42};
+
+// fetchISSFlyOverTimes(coords, (err, times) => {
+//   if (err) {
+//     console.log(err);
+//   } else {
+//     console.log(times);
+//   }
+// });
+
+nextISSTimesForMyLocation(fetchMyIP((err, IP) => {
+  if (err) {
+    console.log(err);
+    return;
   } else {
-    console.log(IP.ip)
+    console.log(IP);
+    fetchCoordsByIP(IP, (err, location) => {
+        if (err) {
+          console.log(err);
+          return
+        } else {
+          console.log(location);
+          fetchISSFlyOverTimes(location, (err, times) => {
+            if (err) {
+              console.log(err);
+            } else {
+              console.log(times);
+              return times;
+            }
+          });
+        }
+      })
   }
-});
+}));
--- iss.js
+++ iss.js
@@ -1,13 +1,58 @@
 const request = require("request");
 
-const fetchMyIP = function (callback) {
+const fetchMyIP = function(callback) {
   request("https://api.ipify.org?format=json", (error, response, body) => {
     if (error) {
-      return callback("Error: " + error, null);
+      callback("Error: " + error, null);
+      return;
+    }
+    if (response.statusCode !== 200) {
+      const msg = `Status Code ${response.statusCode} when fetching IP.  Response: ${body}.`;
+      callback(Error(msg), null);
+      return;
     } else {
-      return callback(null, JSON.parse(body.ip));
+      callback(null, JSON.parse(body).ip);
+      return;
     }
   });
 };
 
-module.exports = { fetchMyIP }
+const fetchCoordsByIP = (ip, callback) => {
+  request(`https://freegeoip.app/json/${ip}`, (error, response, body) => {
+    if (error) {
+      callback("Error while fetching coordinates: " + error, null);
+      return;
+    }
+    if (response.statusCode !== 200) {
+      const msg = `Status Code ${response.statusCode} when fetching coordinates.  Response: ${body}.`;
+      callback(msg, null);
+    } else {
+      const { longitude, latitude } = JSON.parse(body);
+      callback(null, { longitude, latitude });
+    }
+  });
+};
+
+const fetchISSFlyOverTimes = (obj, callback) => {
+  request(`http://api.open-notify.org/iss/v1/?lat=${obj['latitude']}&lon=${obj['longitude']}`, (error, response, body) => {
+    if (error) {
+      callback("Error fetching fly-by times: " + error, null);
+      return;
+    }
+    if (response.statusCode !== 200) {
+      const msg = `Status Code ${response.statusCode} when looking for fly by times. Response ${body}`;
+      callback(msg, null);
+      return;
+    } else {
+      callback(null, JSON.parse(body).response);
+    }
+  });
+};
+
+const nextISSTimesForMyLocation = (callback) => {
+  callback
+  }
+
+
+
+module.exports = { fetchMyIP, fetchCoordsByIP, fetchISSFlyOverTimes, nextISSTimesForMyLocation };
